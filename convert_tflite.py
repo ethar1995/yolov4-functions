1da31e45ccc7aba9956a001ff39bb38411426db2
Ethar Almahmoud <71302338+ethar1995@users.noreply.github.com>
2021-07-02 22:12:12 +0300

    Update convert_tflite.py

--- convert_tflite.py
+++ convert_tflite.py
@@ -8,7 +8,7 @@ import core.utils as utils
 import os
 from core.config import cfg
 
-flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
+flags.DEFINE_string('weights', '/content/yolov4-functions/checkpoints/yolov4-416', 'path to weights file')
 flags.DEFINE_string('output', './checkpoints/yolov4-416-fp32.tflite', 'path to output')
 flags.DEFINE_integer('input_size', 416, 'path to output')
 flags.DEFINE_string('quantize_mode', 'float32', 'quantize mode (int8, float16, float32)')
